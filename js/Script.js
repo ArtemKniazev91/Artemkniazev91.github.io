df6e0ac7ceceb2a75bc461e8ad46fed11601ed1e
Artemkniazev91 <artemkniazev91@gmail.com>
2023-08-24 19:38:24 +0300

    HomeWork2408

--- js/Script.js
+++ js/Script.js
@@ -45,7 +45,7 @@ $(document).ready(function(){
 });
 
 
-var $animation_elements = $('.colors_block, .how_block, .delivery_block, .line, .form_descr');
+var $animation_elements = $('.colors_block, .how_block, .delivery_block, .line, .form_descr, .advantages_item');
 var $window = $(window);
 
 function check_if_in_view() {
